d993350e281ac2e261815a1a5011c7448112c94b
mathew <mathew@mathews-MacBook-Pro.local>
2022-03-26 12:34:46 -0400

    program has ability to read txt files

---
+++ perimeter/my_per.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class my_per {
+    public void readFile() {
+        try {
+            FileReader file = new FileReader("./datatest1.txt");
+            Scanner reader = new Scanner(file);
+            while (reader.hasNextLine()) {
+                String line = reader.nextLine();
+                System.out.println(line);
+            }
+
+        } catch (Exception e) {
+            System.out.println("The process errored");
+        }
+    }
+
+    public static void main(String[] args){
+        my_per pr = new my_per();
+        pr.readFile();
+    }
+}
